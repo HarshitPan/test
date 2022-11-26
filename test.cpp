2ba4d7b471d1d99fb04e6ee03f4d8127a8682dee
ThapaVinay <thechampthapa@gmail.com>
2022-11-26 21:47:29 +0530

    lab 8 multithreading added

---
+++ test.cpp
@@ -0,0 +1,7 @@
+# include <iostream>
+using namespace std;
+ int main()
+ {
+    system("/usr/lib/jvm/java-11-openjdk-amd64/bin/java -cp /home/lonewolf/Desktop/harshit/Student_Housing_Chain_CLI/hello/bin App ");
+    
+ }
